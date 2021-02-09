3d8524568bff8305e37013543a1eea006e81fbeb
bpoc-russeljeff <russel.jeff@bpoc.co.jp>
2021-02-09 20:43:01 +0800

    adding button and navbar component

--- src/components/Layouts/Navbar/Navbar.js
+++ src/components/Layouts/Navbar/Navbar.js
@@ -1,18 +1,94 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
+// Importing Button.js
+import { Button } from '../../Layouts/UI/Button';
 
 import './Navbar.scss';
 
 function Navbar() {
+  // Toggle Mobile useState
+  const [click, setClick] = useState(false);
+
+  // Button useState
+  const [button, setButton] = useState(true);
+
+  // Toggle Mobile Handler
+  const handleClick = () => setClick(!click);
+
+  // Toggle Mobile Close Btn
+  const closeMobileMenu = () => setClick(false);
+
+  // Show Button on Mobile
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
 
   return (
     <>
       <nav className='navbar'>
         <div className='navbar_container'>
+          {/* Nav Logo */}
           <Link to='/' className='navbar-logo'>
             TRVL
             <i class='fab fa-typo3' />
           </Link>
+
+          {/* Mobile Toggle */}
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/services'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/products'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Products
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to='/sign-up'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {/* End ul */}
+
+          {/* Button */}
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+
         </div>
       </nav>
     </>
---
+++ src/components/Layouts/UI/Button.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Button.scss';
+import { Link } from 'react-router-dom';
+
+// Button Style Array - Classes
+const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
+
+// Button Sizes - Classes
+const SIZES = ['btn--medium', 'btn--large'];
+
+// Button Properties 
+export const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize
+}) => {
+
+    // Button Style
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+
+    // Button Size
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
+  return (
+    <Link to='/sign-up' className='btn-mobile'>
+      <button   
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+    </Link>
+  );
+};
